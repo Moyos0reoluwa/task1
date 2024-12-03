3083b1eeeec04bfebf0b385620a3c6e1dafc2268
moyos0reoluwa <oreoluwaoyedele2@gmail.com>
2024-12-03 22:18:11 +0100

    upload to pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
